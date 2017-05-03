8789b38925265a7d28982b30b24e09a956d23618
David Pham <davidpham@Davids-MacBook-Pro.local>
2017-05-03 11:31:18 -0400

    Subclass RCTEventEmitter

--- RNMotionManager/DeviceMotion.h
+++ RNMotionManager/DeviceMotion.h
@@ -4,10 +4,11 @@
 //  Created by Patrick Williams in beautiful Seattle, WA.
 //
 
+#import "RCTEventEmitter.h"
 #import "RCTBridgeModule.h"
 #import <CoreMotion/CoreMotion.h>
 
-@interface DeviceMotion : NSObject <RCTBridgeModule> {
+@interface DeviceMotion : RCTEventEmitter <RCTBridgeModule> {
   CMMotionManager *_motionManager;
 }
 - (void) startDeviceMotionUpdates;
--- RNMotionManager/DeviceMotion.m
+++ RNMotionManager/DeviceMotion.m
@@ -4,15 +4,15 @@
 //  Created by Patrick Williams in beautiful Seattle, WA.
 //
 
-#import <React/RCTBridge.h>
-#import "RCTEventDispatcher.h"
 #import "DeviceMotion.h"
 
 @implementation DeviceMotion
 
-@synthesize bridge = _bridge;
-
 RCT_EXPORT_MODULE();
+
+- (NSArray<NSString *> *)supportedEvents {
+    return @[@"DeviceMotionData"];
+}
 
 - (id) init {
     self = [super init];
@@ -20,11 +20,11 @@ RCT_EXPORT_MODULE();
 
     if (self) {
         self->_motionManager = [[CMMotionManager alloc] init];
-        //Gyroscope
+        //Device motion
         if([self->_motionManager isDeviceMotionAvailable])
         {
             NSLog(@"DeviceMotion available");
-            /* Start the gyroscope if it is not active already */
+            /* Start the device motion if it is not active already */
             if([self->_motionManager isDeviceMotionActive] == NO)
             {
                 NSLog(@"DeviceMotion active");
@@ -44,7 +44,7 @@ RCT_EXPORT_METHOD(startDeviceMotionUpdates) {
     NSLog(@"startDeviceMotionUpdates");
     [self->_motionManager startDeviceMotionUpdates];
 
-    /* Receive the gyroscope data on this block */
+    /* Receive the device motion data on this block */
     [self->_motionManager startDeviceMotionUpdatesToQueue:[NSOperationQueue mainQueue]
                                       withHandler:^(CMDeviceMotion *deviceMotionData, NSError *error)
      {
@@ -54,14 +54,14 @@ RCT_EXPORT_METHOD(startDeviceMotionUpdates) {
          double timestamp = deviceMotionData.timestamp;
          NSLog(@"startDeviceMotionUpdates: %f, %f, %f, %f", roll, pitch, yaw, timestamp);
 
-         [self.bridge.eventDispatcher sendDeviceEventWithName:@"DeviceMotionData" body:@{
-                                                                                 @"attitude": @{
-                                                                                         @"roll" : [NSNumber numberWithDouble:roll],
-                                                                                         @"pitch" : [NSNumber numberWithDouble:pitch],
-                                                                                         @"yaw" : [NSNumber numberWithDouble:yaw],
-                                                                                         @"timestamp" : [NSNumber numberWithDouble:timestamp]
-                                                                                         }
-                                                                                 }];
+         [self sendEventWithName:@"DeviceMotionData" body:@{
+                                                            @"attitude": @{
+                                                                    @"roll" : [NSNumber numberWithDouble:roll],
+                                                                    @"pitch" : [NSNumber numberWithDouble:pitch],
+                                                                    @"yaw" : [NSNumber numberWithDouble:yaw],
+                                                                    @"timestamp" : [NSNumber numberWithDouble:timestamp]
+                                                                    }
+                                                            }];
      }];
 
 }
